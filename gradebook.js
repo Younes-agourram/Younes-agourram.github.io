7eacc5dece85d99532e52ec0b246314a64ebf36f
Younes AGOURRAM <agourram.younes94@gmail.com>
2025-03-19 18:35:45 -0400

    gradebook

---
+++ gradebook.js
@@ -0,0 +1,9 @@
+// Stub function to fetch data (to be completed in the future)
+function fetchGradeData() {
+  console.log("fetchGradeData called");
+}
+
+// Stub function to populate the gradebook table (to be completed in the future)
+function populateGradebook() {
+  console.log("populateGradebook called");
+}
