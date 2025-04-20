01031a8a26b06ebdd1b2f17c2a8864e33d18f892
Younes AGOURRAM <agourram.younes94@gmail.com>
2025-04-20 15:40:05 -0400

    final

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,9 +1,50 @@
 // Stub function to fetch data (to be completed in the future)
 function fetchGradeData() {
-  console.log("fetchGradeData called");
+  console.log("Fetching grade data...");
+  // Create a new request for HTTP data
+  let xhr = new XMLHttpRequest();
+  // This is the address on the machine we're asking for data
+  let apiRoute = "/api/grades";
+  // When the request changes status, we run this anonymous function
+  xhr.onreadystatechange = function(){
+    let results;
+    // Check if we're done
+    if(xhr.readyState === xhr.DONE){
+      // Check if we're successful
+      if(xhr.status !== 200){
+        console.error(`Could not get grades. Status: ${xhr.status}`);
+      }
+      // And then call the function to update the HTML with our data
+      populateGradebook(JSON.parse(xhr.responseText));
+    }
+  }.bind(this);
+  xhr.open("get", apiRoute, true);
+  xhr.send();
 }
 
 // Stub function to populate the gradebook table (to be completed in the future)
-function populateGradebook() {
-  console.log("populateGradebook called");
+function populateGradebook(data) {
+  // This function will take the fetched grade data and populate the table
+  console.log("Populating gradebook with data:", data);
+  let tableElm = document.getElementById("gradebook"); // Get the gradebook table element
+  data.forEach(assignment => { // For each row of data we're passed in
+    let row = document.createElement("tr"); // create a table row element
+    let columns = []]; // Handy place to stick the columns of information
+    columns.name = document.createElement('td'); // The first column's table data will be the name
+    columns.name.appendChild(
+      // Concatenate the full name
+      document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+    );
+    columns.grade = document.createElement('td'); // second column will be the grade
+    columns.grade.appendChild(
+      // We just put the name in text, you could be fancy and figure out the letter grade here
+      // with either a bunch of conditions, or a JavaScript "switch" statement
+      document.createTextNode(assignment.total_grade)
+    );
+    // Add the table data columns to the table row
+    row.appendChild(columns.name);
+    row.appendChild(columns.grade);
+    // Add the row to the table itself to make the data visible
+    tableElm.appendChild(row);
+  });
 }
